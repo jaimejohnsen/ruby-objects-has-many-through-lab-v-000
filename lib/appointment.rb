97d477f1b7658c8f8e89bca045030563605e98f2
Jaime Johnsen <drivetraindiva@gmail.com>
2020-09-15 17:12:55 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -8,9 +8,10 @@ class Appointment
   end
 
   def initialize(date, patient, doctor)
-  @date = date
-  @doctor = doctor
-  @@all << self
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
   end
 
 end
